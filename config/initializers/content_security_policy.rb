32c3762a668006f9e1b7b7404d507ee42f37d766
Andy W <Andy.Kee.Wong@gmail.com>
2018-11-11 01:54:35 -0500

    Initializing vue.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -25,5 +25,10 @@
 # Rails.application.config.content_security_policy_report_only = true
 
 Rails.application.config.content_security_policy do |policy|
-  policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035' if Rails.env.development?
+  allowed_sources = :self, :https
+  if Rails.env.development?
+    policy.connect_src *allowed_sources, 'http://localhost:3035', 'ws://localhost:3035'
+    allowed_sources << :unsafe_eval
+  end
+  policy.script_src *allowed_sources
 end
