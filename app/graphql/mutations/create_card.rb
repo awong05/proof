277d33d01bdaca016acc328a094d9b19b2d1d521
Andy W <Andy.Kee.Wong@gmail.com>
2018-11-25 15:07:18 -0500

    Basic card mutation.

---
+++ app/graphql/mutations/create_card.rb
@@ -0,0 +1,23 @@
+module Mutations
+  class CreateCard < Mutations::BaseMutation
+    argument :overview, String, required: true
+
+    field :card,   Types::CardType, null: true
+    field :errors, [String],        null: false
+
+    def resolve overview:
+      card = Card.new overview: overview
+      if card.save
+        {
+          card:   card,
+          errors: []
+        }
+      else
+        {
+          comment: nil,
+          errors:  card.errors.full_messages
+        }
+      end
+    end
+  end
+end
