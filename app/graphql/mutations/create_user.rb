ff3517b37c63ed0b73394f224a1505433896097f
Andy W <Andy.Kee.Wong@gmail.com>
2018-11-25 20:01:12 -0500

    Debugging.

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,12 +1,18 @@
 module Mutations
   class CreateUser < BaseMutation
+    argument :name,          String,                       required: true
     argument :auth_provider, Types::AuthProviderInputType, required: true
 
     field :user,   Types::UserType, null: true
     field :errors, [String],        null: false
 
-    def resolve auth_provider:
-      user = User.new email: auth_provider.email, password: auth_provider.password
+    def resolve name:, auth_provider:
+      user = User.new(
+        name: name,
+        email: auth_provider.email,
+        password: auth_provider.password
+      )
+
       if user.save
         {
           user:   user,
